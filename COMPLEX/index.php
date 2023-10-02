6d2d8a27f7101a3d7f8d7ce6892c7b4e68e24868
Чеканов Антон <kopholgol@gmail.com>
2023-10-02 13:07:19 +0300

    lol

--- COMPLEX/index.php
+++ COMPLEX/index.php
@@ -15,11 +15,24 @@
 ?>
 
 <ul>
-    <li class="item1"></li>
-    <li class="item2"></li>
-    <li class="item3"></li>
-    <li class="item4"></li>
-    <li class="item5"></li>
+   <li class="item1"></li>
+   <li class="item1"></li>
+   <li class="item1"></li>
+   <li class="item2"></li>
+   <li class="item2"></li>
+   <li class="item2"></li>
+   <li class="item1"></li>
+   <li class="item1"></li>
+   <li class="item1"></li>
+   <li class="item2"></li>
+   <li class="item2"></li>
+   <li class="item2"></li>
+   <li class="item1"></li>
+   <li class="item1"></li>
+   <li class="item1"></li>
+   <li class="item2"></li>
+   <li class="item2"></li>
+   <li class="item2"></li>
 </ul>
 
 <a href="#" class="btn">Hover Me</a>
